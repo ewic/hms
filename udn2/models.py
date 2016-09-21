4dcf4c921ba6d5a5feafbb698d4bafa8bbbfc426
Eric Zhang <ewicexclamationpoint@gmail.com>
2016-09-21 16:59:16 -0400

    Changed model text field to be more clear

--- udn2/models.py
+++ udn2/models.py
@@ -5,9 +5,11 @@ import json
 # Create your models here.
 class Participant(models.Model):
 	name = models.CharField(max_length=200)
-	birthdate = models.DateField('birthdate')
+	birthdate = models.DateField('birthdate (mm/dd/yyyy')
 	siblings = models.BooleanField()
 
+	## Eventually this should be some kind of list structure, so we could
+	##   do analytics on them.
 	environmental_exposures = models.TextField()
 	genetic_mutations = models.TextField()
 
@@ -30,10 +32,11 @@ class Participant(models.Model):
 		age = now.year - self.birthdate.year
 		return age
 
+	# Didn't end up storing these as JSON strings, so not using them
 	# Getters for these because Django doesn't support
 	#   any native list-like or dictionary-like storage
 	#   in SQLite (for now)
-	def getEnvironmentalExposures(self):
-		return json.loads(self.environmental_exposures)
-	def getGeneticMustations(self):
-		return json.loads(self.genetic_mutations)
+	# def getEnvironmentalExposures(self):
+	# 	return json.loads(self.environmental_exposures)
+	# def getGeneticMustations(self):
+	# 	return json.loads(self.genetic_mutations)
